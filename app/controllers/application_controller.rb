a78b8f73f9df8ddd3c37fb55e63b4a2481a08f45
Robert Salaiz <work.salaizrm@gmail.com>
2020-09-02 23:30:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,57 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+      id = params[:id]
+         new_params = {}
+         old = Recipe.find(id)
+         new_params[:name] = params[:name]
+         new_params[:ingredients] = params[:ingredients]
+         new_params[:cook_time] = params[:cook_time]
+         old.update(new_params)
+
+      redirect to "/recipes/#{@recipes.id}"
+    end
+
+  post '/recipes' do
+    @recipes = Recipe.create(params)
+    redirect to "/recipes/#{@recipes.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipes = Recipe.find_by_id(params[:id])
+    @recipes.delete
+    redirect to '/recipes'
+  end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+Recipe.create(name: 'Bomb.com Mac and Cheese', ingredients: "milk, butter, cheese, elbow pasta", cook_time: "20 minutes")
